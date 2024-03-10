429ae98b76f4b913c64ecf0f9ae07441802bfe61
TsaiPeiChen <1022tiffany@gmail.com>
2024-03-10 10:37:53 +0800

    Add files via upload

---
+++ VAE-GAN/makedata.py
@@ -0,0 +1,37 @@
+import torch.nn as nn
+from einops import rearrange
+from PIL import Image
+
+import os
+import shutil
+from glob import glob
+from torch.optim import Adam
+from torch.nn import CrossEntropyLoss
+
+from tqdm import tqdm 
+from torchvision import transforms, datasets
+from torchvision.transforms import Resize, ToTensor
+
+import torchvision.transforms as transforms
+import torch
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from torch.utils.data import DataLoader, Dataset, ConcatDataset, Subset
+
+if __name__=="__main__":
+    data_entry = pd.read_csv('rsna-pneumonia-detection-challenge/stage_2_detailed_class_info.csv')
+    data_entry.columns = ['Id', 'class']
+    data_entry['Id'] = data_entry['Id'].apply(lambda x: x + '.dcm')
+    
+    my_glob = glob('rsna-pneumonia-detection-challenge/stage_2_train_images/*.dcm')
+    all_image_paths = {os.path.basename(x): x for x in my_glob}
+    data_entry['full_path'] = data_entry['Id'].map(all_image_paths.get)
+    data_entry = data_entry.dropna(subset=['full_path'])
+    data_entry['label'] = data_entry['class'].apply(lambda target: 0 if target == 'Normal' else 1)
+    data_entry = data_entry.drop(columns=['Id', 'class'])
+    
+    data_entry.to_csv('RSNA.csv', index=False)
+    print(data_entry)
